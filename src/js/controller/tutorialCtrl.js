d8920791dc0b9905a36445ec36a16a4b35663539
amandapersampa <amandapersampa@hotmail.com>
2016-12-17 11:51:18 -0200

    two way data biding

--- src/js/controller/tutorialCtrl.js
+++ src/js/controller/tutorialCtrl.js
@@ -1,5 +1,13 @@
-angular.module('tutorialCtrlModule',[]);
+angular.module('tutorialCtrlModule',[])
+.controller('TutorialCtrl',
+    ["$scope",function ($scope){
+        {
+            $scope.tutorialObject = {};
+            $scope.tutorialObject.title = "Main Page";
+            $scope.tutorialObject.subTitle = "Sub Title";
+            $scope.tutorialObject.bindOutput = 2;
+            $scope.tutorialObject.fname = "Amanda";
+            $scope.tutorialObject.lname = "Sampaio";
+        }
+    }]);
 
-.controller("TutorialCtrl",["&scope", function(&scope)]);
-
-.service();
